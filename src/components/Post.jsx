f6d389772564117611121a08c8711e86d1d7682b
Mateus Barcelos <mateuspit@hotmail.com>
2023-02-09 13:37:05 -0300

    Creation off CSS animation and functions to like-post, save-post, double-click-like-post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,40 @@
 export default function Post(props){
+
+    let savePostMode = "bookmark-outline";
+    let likePostMode = "heart-outline";
+
+    function savePost(){
+        //<ion-icon name="bookmark"></ion-icon>
+        // alert("post");
+        if(savePostMode === "bookmark-outline"){
+            savePostMode = "bookmark";
+        }
+        else if (savePostMode === "bookmark"){
+            savePostMode = "bookmark-outline";
+        }
+        // {savePostMode === "bookmark" ? "bookmark-outline" : "bookmark" }
+    }
+
+    function likePost(){
+        //<ion-icon name="heart"></ion-icon>
+        // alert("coração");
+        if(likePostMode === "heart-outline"){
+            likePostMode = "heart";
+            props.likes = props.likes + 1;
+        }
+        else if (likePostMode === "heart"){
+            likePostMode = "heart-outline";
+            props.likes = props.likes - 1;
+        }
+        // {likePostMode === "heart" ? "heart-outline" : "heart" }
+    }
+
+    function doubleClickLike(){
+        document.querySelector(".like-heart").classList.add("like-heart");
+        if(likePostMode === "heart-outline"){
+            likePostMode = "heart";
+        }             
+    }
     return(
         // imageUser : "./assets/meowed 1.png",
         // title : "meowed",
@@ -17,16 +53,19 @@ export default function Post(props){
                 </div>             
             </div>
             <div class="post-photo">
-                <img src={props.photo} alt="" width="100px"/>
+                <img onDoubleClick={doubleClickLike} src={props.photo} alt="" width="100px"/>
+                <div class="like-heart">
+                    <ion-icon name="heart"></ion-icon>                                        
+                </div>
             </div>
             <div class="post-buttons">
                 <div class="post-left-buttons">
-                    <ion-icon name="heart-outline"></ion-icon>
+                    <div class="likeButton"><ion-icon onClick={likePost} name={likePostMode}></ion-icon></div>
                     <ion-icon name="chatbubble-outline"></ion-icon>
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div class="post-right-button">
-                    <ion-icon name="bookmark-outline"></ion-icon>                    
+                    <ion-icon onClick={savePost} name={savePostMode}></ion-icon>                   
                 </div>
             </div>
             <div class="post-stats">
--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,23 +1,36 @@
 export default function Usuario(){
-    let userName = prompt("Por favor, insira o nome de usuario");
-    // let userName = "Mateus";
-    while(userName === "" || userName === null || userName === undefined){
-        userName = prompt("Por favor, insira um nome válido!");
+
+
+    function chanceUserName(){
+        let userName = prompt("Por favor, insira o nome de usuario");
+        // let userName = "Mateus";
+        while(userName === "" || userName === null || userName === undefined){
+            userName = prompt("Por favor, insira um nome válido!");
+        }        
     }
+
+    function chanceUserIcon(){
+        let userIcon = prompt("Por favor, insira uma URL de uma imagem");
+        // let userName = "Mateus";
+        while(userIcon === "" || userIcon === null || userIcon === undefined){
+            userIcon = prompt("Por favor, insira uma URL valida!");
+        }        
+    }
+    
+
+    let userName = "catanacomics";
+    let userIcon = "./assets/catanacomics 1.png";
             
 
     return(
-        
-
         <div class="top-side-menu">
             <div class="top-side-menu-img">
-                <img src="./assets/catanacomics 1.png" alt="" width="100px"/>
+                <img onClick={chanceUserIcon} src={userIcon} alt="" width="100px"/>
             </div>
             <div class="top-side-menu-user">
                 {(userName !== false) && (userName !== "") ? userName : "catanacomics"}
-                {/* Beto */}
             </div>
-            <ion-icon name="pencil-outline"></ion-icon>
+            <ion-icon class="chanceUserName" onClick={chanceUserName} name="pencil-outline"></ion-icon>
         </div>
     );
 }
